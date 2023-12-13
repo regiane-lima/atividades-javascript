35e10f2a8da21b0bf32b793f98e87d9a44b8cdf4
Regiane Lima <120677112+regiane-lima@users.noreply.github.com>
2023-12-13 20:42:46 -0300

    Create atividade.py

---
+++ atividade.py
@@ -0,0 +1,6 @@
+bebida_favorita = 'coca cola'
+print (bebida_favorita)
+print(type(bebida_favorita))
+bebida_preco = 6.99
+print(bebida_preco)
+print('a minha bebida favorita é: ' + bebida_favorita )
